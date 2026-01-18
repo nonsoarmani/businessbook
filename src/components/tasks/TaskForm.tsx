8f7e1b17942c06cc73d38efd09f86906d76a7584
[dyad] <empiricalwriting@gmail.com>
2026-01-18 15:18:46 +0100

    [dyad] Adding debug logging to task components - wrote 4 file(s)

--- src/components/tasks/TaskForm.tsx
+++ src/components/tasks/TaskForm.tsx
@@ -47,6 +47,7 @@ const TaskForm = () => {
         createdAt: format(new Date(), 'yyyy-MM-dd'),
       };
 
+      console.log('Dispatching ADD_TASK action with:', newTask);
       dispatch({ type: 'ADD_TASK', payload: newTask });
       showSuccess('Task added successfully!');
       form.reset({
